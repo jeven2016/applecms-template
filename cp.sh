d83ca066d7662bf396fb586fe540f7596e9b1ff0
jeven2016 <jujucom@126.com>
2018-11-28 18:11:15 +0800

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-docker cp template/default_wap/css/style.css myvm_movie_1:/var/www/html/template/default_wap/css/style.css
-docker cp template/default_wap/html/public/paging.html  myvm_movie_1:/var/www/html/template/default_wap/html/public/paging.html
-docker cp template/default_wap/html/comment/ajax.html  myvm_movie_1:/var/www/html/template/default_wap/html/comment/ajax.html
+docker cp template myvm_movie_1:/var/www/html/
+docker cp tu.php myvm_movie_1:/var/www/html/
 
 
